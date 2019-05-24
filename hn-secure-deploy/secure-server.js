99bfb29c95380ea3bf2fcef84727de014f5620e0
Suru Dissanaike <suru.dissanaike@himinds.com>
2019-05-24 15:08:17 +0200

    Update secure-server.js

--- hn-secure-deploy/secure-server.js
+++ hn-secure-deploy/secure-server.js
@@ -5,16 +5,15 @@ const http = require('http');
 const express = require('express');
 
 // Certificate
-/*
-const privateKey = fs.readFileSync('./keys/privkey1.pem', 'utf8');
-const certificate = fs.readFileSync('./keys/cert1.pem', 'utf8');
 
+const privateKey = fs.readFileSync('/home/certbot/etc/live/<YOUR-DOMAIN>/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/home/certbot/etc/live/<YOUR-DOMAIN>/cert.pem', 'utf8');
 const options = {
 	key: privateKey,
 	cert: certificate,
 //	ca: ca
 };
-*/
+
 
 const app = express();
 app.use(express.static(process.env.SERVE_DIRECTORY || '../hn-app/build'));
@@ -22,8 +21,6 @@ app.get('/', function(req, res) {
   return res.end('<p>This server serves up static files.</p>');
 });
 
-//const httpsServer = https.createServer(options, app);
-const httpServer = http.createServer(app);
+const httpsServer = https.createServer(options, app);
+//const httpServer = http.createServer(app);
 
-//httpsServer.listen(process.env.SERVER_PORT || 5000);
-httpServer.listen(process.env.SERVER_PORT || 5000);
